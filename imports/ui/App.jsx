fe5db81721b050f5d56743144d2f49a9f73a34e6
ange-desk <ange-desk@ange-desks.local>
2016-05-05 10:17:14 +0800

    add Task.jsx

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,11 +1,25 @@
 //注意大小写，使用snippet的时候后面变成了React, 于是出错了，但是meteor也没有报错。
 //是的，但是chrome报错了，如此，运用已经有的工具，meteor就比rails要方便了。
-import React,{Component} from 'React';
+import React,{Component} from 'react';
 
 //这又要载入一个任务UI
-//import Task from './Task.jsx';
+import Task from './Task.jsx';
 
 export default class App extends Component{
+//好理解，因为
+	getTasks(){
+		return [
+			{_id:1,text:"task1"},
+			{_id:2,text:"task2"},
+			{_id:3,text:"task3"},
+		];
+	}
+//标签Task其实是来自于Task.jsx中，另外，剩下的这个task={task}是个啥意思？
+	renderTasks(){
+			return this.getTasks().map((task)=>(
+				<Task key={task._id} task={task}/>
+			))
+	}
 	render(){
 		return(
 			<div class="container">
@@ -13,9 +27,8 @@ export default class App extends Component{
 					<h1>Todo列表</h1>
 				</header>
 				<ul>
-					{}
+					{this.renderTasks()}
 				</ul>
-				<h1>相信我，没错的。</h1>
 			</div>
 			)
 	}
---
+++ imports/ui/Task.jsx
@@ -0,0 +1,14 @@
+import React,{Component, PropTypes} from 'react';
+
+//this.props?看来是继承自PropTypes了，还不太明白干什么的。
+export default class Task extends Component{
+	render() {
+		return(
+		<li>{this.props.task.text}</li>
+		);
+	}
+}
+
+Task.propTypes={
+	task: PropTypes.object.isRequired,
+}
